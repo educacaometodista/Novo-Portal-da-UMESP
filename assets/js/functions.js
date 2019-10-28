36dee3fd54dc5afd7e264c48b97e571f172b2c68
LeuAlmeida <leo@webid.net.br>
2019-10-28 18:20:06 -0300

    Inseração da função randombg

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -14,6 +14,17 @@ $(function() {
   }
 });
 
+
+function randombg(){
+  var random= Math.floor(Math.random() * 4) + 0;
+  var bigSize = ["url('https://portalpreprd.metodista.br/Metodista/++theme++portal-umesp/assets/imagens/bg/umesp1.jpg')",
+                 "url('https://portalpreprd.metodista.br/Metodista/++theme++portal-umesp/assets/imagens/bg/umesp2.jpg')",
+                 "url('https://portalpreprd.metodista.br/Metodista/++theme++portal-umesp/assets/imagens/bg/umesp3.jpg')",
+                 "url('https://portalpreprd.metodista.br/Metodista/++theme++portal-umesp/assets/imagens/bg/umesp3.jpg')"]
+  document.querySelector("body.section-home .outer-wrapper > .container .destaqueshome.mosaic-grid-row-fluid").style.backgroundImage=bigSize[random];
+}
+
+
 // Start of insert news to event
 $(document).ready(function(jq) {
   $("#relatedItemBox a").each(function() {
