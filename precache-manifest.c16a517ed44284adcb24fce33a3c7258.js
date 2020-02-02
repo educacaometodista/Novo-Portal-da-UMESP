0845e41aa01656f8c670fee05bd5972509ee0064
Léu Almeida <leo@webid.net.br>
2020-02-02 16:00:49 -0300

    feat: Improvements and tests

--- precache-manifest.c16a517ed44284adcb24fce33a3c7258.js
+++ precache-manifest.c16a517ed44284adcb24fce33a3c7258.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "af7e4e37c224905a61989fb3d7f3955c",
+    "revision": "146a7959eee3568e3555a523930726fd",
     "url": "/index.html"
   },
   {
-    "revision": "f4dc65e3eb163acae2bb",
-    "url": "/static/css/main.9b011277.chunk.css"
+    "revision": "d7b3efc2ed7988dea1fa",
+    "url": "/static/css/main.551e7f9d.chunk.css"
   },
   {
     "revision": "2d8160111d3000fe1f14",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.b60254bb.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f4dc65e3eb163acae2bb",
-    "url": "/static/js/main.8459a372.chunk.js"
+    "revision": "d7b3efc2ed7988dea1fa",
+    "url": "/static/js/main.e9605b35.chunk.js"
   },
   {
     "revision": "e3057e7fd02fcc1169d7",
