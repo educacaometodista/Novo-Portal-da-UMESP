daf672e6d65156c7b8eefd63dc8961d2956e57f2
Léu Almeida <leo@webid.net.br>
2020-02-02 15:31:19 -0300

    add: Implementing ReactJS monthly prices scripts

---
+++ precache-manifest.1cf78ced341a93d93417abc4525ee5f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af7e4e37c224905a61989fb3d7f3955c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4dc65e3eb163acae2bb",
+    "url": "/static/css/main.9b011277.chunk.css"
+  },
+  {
+    "revision": "2d8160111d3000fe1f14",
+    "url": "/static/js/2.b60254bb.chunk.js"
+  },
+  {
+    "revision": "029872d0b314944c339c7b0d2b6cd40b",
+    "url": "/static/js/2.b60254bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4dc65e3eb163acae2bb",
+    "url": "/static/js/main.8459a372.chunk.js"
+  },
+  {
+    "revision": "e3057e7fd02fcc1169d7",
+    "url": "/static/js/runtime-main.8fd1114d.js"
+  }
+]);
